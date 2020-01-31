7671732bc3af78291b92b86f10ac1836c1f49448
Pourya Hoseini <sph.alinodehi@gmail.com>
2020-01-31 15:30:37 -0800

    Minor changes

--- Dataset_Consolidation.py
+++ Dataset_Consolidation.py
@@ -2,7 +2,7 @@ import shutil
 import os
 import numpy as np
 
-RELATIVE_SPLITTING_ADDRESS = "2P/10x10/"
+RELATIVE_SPLITTING_ADDRESS = "2P/4x4/"
 ROOT_ADDRESS = "/home/pourya/Documents/NashNet/Datasets/"
 SPLIT_FILES_FOLDER = "/Split_Files/"
 ORIGINAL_FILES_FOLDER = "Individual_Games/"
