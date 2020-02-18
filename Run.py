9c7a4d1705d8cb3e6ccaa47130adea1b17e7a116
Pourya Hoseini <sph.alinodehi@gmail.com>
2020-02-18 00:35:07 -0800

    A fix to enable running on Windows by adding a main function and calling that

--- Run.py
+++ Run.py
@@ -1,21 +1,26 @@
 import sys
 from NashNet import NashNet
 
-# Decide on the config file address
-if len(sys.argv) == 1:
-    config_file_address = './Config/Config.cfg'
-else:
-    config_file_address = sys.argv[1]
 
-# Build a network instance
-nash_net = NashNet(config_file_address, configSection="DEFAULT")
+def main():
+    # Decide on the config file address
+    if len(sys.argv) == 1:
+        config_file_address = './Config/Config.cfg'
+    else:
+        config_file_address = sys.argv[1]
 
-# Train the network
-nash_net.train()
+    # Build a network instance
+    nash_net = NashNet(config_file_address, configSection="DEFAULT")
 
-# Evaluate the network
-nash_net.evaluate(num_to_print=None)
+    # Train the network
+    nash_net.train()
 
-# Only print some examples
-nash_net.printExamples(num_to_print=None)
+    # Evaluate the network
+    nash_net.evaluate(num_to_print=None)
 
+    # Only print some examples
+    # nash_net.printExamples(num_to_print=None)
+
+
+if __name__ == "__main__":
+    main()
